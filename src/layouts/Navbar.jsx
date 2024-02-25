5358d38cd5417cdc6695ba5135930d9dcd4ae95f
fraciah <karfraciah@gmail.com>
2024-02-25 22:48:55 +0300

    Add scroll effect to navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,28 +1,46 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import "./styles.css";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+  
+    //listen for scroll events
+    window.addEventListener('scroll', handleScroll);
+  
+    //cleanup function to remove event listener
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className={`navbar sticky-top navbar-expand-lg bg-white py-1 ${scrolled ? 'scrolled-navbar' : ''}`}>
       <div className="container-fluid">
-        <img src={logo} className="img-fluid w-25" alt="logo"/>
-        {/* <a className="navbar-brand" href="#">Result Analysis</a> */}
+        <div className="logo-container">
+          <img src={logo} className="img-fluid" alt="logo"/>
+        </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/" className="nav-link active">Home</Link>
-              {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+              <Link to="/" className="nav-link me-4 fw-semibold">Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/students" className="nav-link">Students</Link>
-              {/* <a className="nav-link" href="#">Students</a> */}
+              <Link to="/students" className="nav-link me-4 fw-semibold">Students</Link>
             </li>
             <li className="nav-item">
-              <Link to="/teachers" className="nav-link">Teachers</Link>
-              {/* <a className="nav-link" href="#">Teachers</a> */}
+              <Link to="/teachers" className="nav-link me-4 fw-semibold">Teachers</Link>
             </li>
           </ul>
         </div>
