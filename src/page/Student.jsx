ea23a12d87e8ffa9dab860efd51428f25ca93788
fraciah <karfraciah@gmail.com>
2024-02-25 11:36:30 +0300

    Add student data visualization

--- src/page/Student.jsx
+++ src/page/Student.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { Bar } from "react-chartjs-2";
+
+import { students } from "../utils/Data/Students";
+import { studentTermScores, options } from "../utils/Visuals/StudentTermScores";
 
 const Student = () => {
+  const urlParams = useParams();
+  // console.log(urlParams);
+
+  // Convert the student id to a number to match the data type in the students array
+  const studentId = Number(urlParams.id);
+  // console.log(typeof studentId);
+
+  const student = students?.students?.find(
+    (student) => student.id == studentId
+  );
+  // console.log(student);
+  const [selectedSubject, setSelectedSubject] = useState("math");
+  const data = studentTermScores(student, selectedSubject);
+
   return (
     <div>
-      <p>Student</p>
+      <p>Student data to be here</p>
+      <div className="form-group row align-items-center">
+        <label className="col-sm-2 col-form-label">Select subject:</label>
+        <div className="col-sm-2">
+          <select className="form-select"  value={selectedSubject} onChange={(e) => setSelectedSubject(e.target.value)}>
+            <option value="math">Math</option>
+            <option value="english">English</option>
+            <option value="history">History</option>
+            <option value="chemistry">Chemistry</option>
+            <option value="physics">Physics</option>
+          </select>
+        </div>
+      </div>
+      <Bar data={data} options={options}/>
     </div>
   )
 }
 
-export default Student
+export default Student;
---
+++ src/utils/Visuals/StudentTermScores.js
@@ -0,0 +1,32 @@
+export const getStudentScoresBySubject = (student, subject) => {
+    return [
+        student.term1_scores[subject],
+        student.term2_scores[subject],
+        student.term3_scores[subject],
+        student.term4_scores[subject],
+    ]
+}
+
+export const studentTermScores = (student, selectedSubject) => {
+    return {
+        labels: ['Term 1', 'Term 2', 'Term 3', 'Term 4'],
+        datasets: [
+            {
+                label: `Performance in ${selectedSubject.charAt(0).toUpperCase() + selectedSubject.slice(1)}`,
+                data: getStudentScoresBySubject(student, selectedSubject),
+                backgroundColor: 'pink',
+                borderColor: 'black',
+                borderWidth: 1,
+            }
+        ],
+    }
+}
+
+export const options={
+    scales: {
+      y: {
+        min: 0,
+        max: 100
+      }
+    }
+}
