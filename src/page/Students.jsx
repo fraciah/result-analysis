0e3347ae5d88835e075a0ab396efad45cc54142f
fraciah <karfraciah@gmail.com>
2024-02-26 22:08:19 +0300

    Style Students Page

--- src/page/Students.jsx
+++ src/page/Students.jsx
@@ -9,36 +9,42 @@ const Students = () => {
   };
 
   return (
-    <div className="table-responsive">
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Admission Number</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Other Names</th>
-            <th scope="col">Gender</th>
-            <th scope="col">P/ G Contact</th>
-            <th scope="col">P/ G Email</th>
-            <th scope="col">Address</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students?.students?.map((student, index) => {
-            return (
-              <tr key={student.id} onClick={() =>handleRowClick(student.id)}>
-                <th>{index + 1}</th>
-                <td>{student.admnNo}</td>
-                <td>{student.fName}</td>
-                <td>{student.oNames}</td>
-                <td>{student.gender}</td>
-                <td>{student.p_contact}</td>
-                <td>{student.p_email}</td>
-                <td>{student.address}</td>
-              </tr>
-            )})}
-        </tbody>
-      </table>
+    <div className="container">
+      <h2 className="fw-bold blue fs-3 text-center my-4">Student Records</h2>
+      <p className="text-center mb-4">
+        This table provides a comprehensive view of all students&apos; information. Click on a row to view more details about a student.
+      </p>
+      <div className="table-responsive">
+        <table className="table mx-auto table-light table-borderless table-hover">
+          <thead className="table-primary text-center">
+            <tr>
+              <th scope="col"></th>
+              <th scope="col" className="fw-semibold">Admission Number</th>
+              <th scope="col" className="fw-semibold">First Name</th>
+              <th scope="col" className="fw-semibold">Other Names</th>
+              <th scope="col" className="fw-semibold">Gender</th>
+              <th scope="col" className="fw-semibold">P/ G Contact</th>
+              <th scope="col" className="fw-semibold">P/ G Email</th>
+              <th scope="col" className="fw-semibold">Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students?.students?.map((student, index) => {
+              return (
+                <tr className="text-center" style={{cursor: "pointer"}} key={student.id} onClick={() =>handleRowClick(student.id)}>
+                  <th className="fw-semibold">{index + 1}</th>
+                  <td>{student.admnNo}</td>
+                  <td>{student.fName}</td>
+                  <td>{student.oNames}</td>
+                  <td>{student.gender}</td>
+                  <td>{student.p_contact}</td>
+                  <td>{student.p_email}</td>
+                  <td>{student.address}</td>
+                </tr>
+              )})}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
